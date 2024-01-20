c80696a5aca1c302f85d487256999bbe70076cf9
Silin Song <silin5@ualberta.ca>
2024-01-19 23:25:54 -0700

    Signup page setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
 import Login from './Login';
+import SignUp from './Signup';
+
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
